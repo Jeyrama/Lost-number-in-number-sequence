187fe62abd57f96b5aa6f2ef354e5a0200b84fe3
Jeyrama <126204426+Jeyrama@users.noreply.github.com>
2023-06-21 10:47:18 -0700

    Update Lost-number-in-number-sequence.js

--- Lost-number-in-number-sequence.js
+++ Lost-number-in-number-sequence.js
@@ -26,3 +26,11 @@ function findDeletedNumber(arr, mixArr) {
 
 // or
 
+function findDeletedNumber(arr, mixArr) {
+  for(let i = 0; i < arr.length; i++) {
+    if(mixArr.indexOf(arr[i]) < 0) {
+      return arr[i];
+    }
+  }
+  return 0;
+}
